fd376a1e4bc524ecf630dccc8d98c952fbf48515
Eriquy Jhordan <eriquy@gmail.com>
2023-05-12 13:05:37 -0300

    fix: number height

--- src/components/Differentials/index.tsx
+++ src/components/Differentials/index.tsx
@@ -15,31 +15,31 @@ export default function Differentials({ professorId, professorName }: IDifferent
           <h2 className={styles.title}>Abra sua conta 100% gratuita e opere com o maior banco de investimentos da América latina, o BTG Pactual.</h2>
           <ul className={styles.listContent}>
             <li className={styles.listitem}>
-              <MdCheck className={styles.check} size={24} />
+              <MdCheck className={styles.check} size={20} />
               <span className={styles.listText}>Atendimento personalizado com um assessor de investimentos especialista em renda variável;</span>
             </li>
             <li className={styles.listitem}>
-              <MdCheck className={styles.check} size={24} />
+              <MdCheck className={styles.check} size={20} />
               <span className={styles.listText}>Condições únicas de corretagem;</span>
             </li>
             <li className={styles.listitem}>
-              <MdCheck className={styles.check} size={24} />
+              <MdCheck className={styles.check} size={20} />
               <span className={styles.listText}>Plataformas gratuitas;</span>
             </li>
             <li className={styles.listitem}>
-              <MdCheck className={styles.check} size={24} />
+              <MdCheck className={styles.check} size={20} />
               <span className={styles.listText}>Relatórios e recomendações diárias da maior casa de análise do Brasil;</span>
             </li>
             <li className={styles.listitem}>
-              <MdCheck className={styles.check} size={24} />
+              <MdCheck className={styles.check} size={20} />
               <span className={styles.listText}>Grupo no telegram exclusivo com notícias e acompanhamento de mercado em tempo real;</span>
             </li>
             <li className={styles.listitem}>
-              <MdCheck className={styles.check} size={24} />
+              <MdCheck className={styles.check} size={20} />
               <span className={styles.listText}>Calculadora de imposto de renda gratuita;</span>
             </li>
             <li className={styles.listitem}>
-              <MdCheck className={styles.check} size={24} />
+              <MdCheck className={styles.check} size={20} />
               <span className={styles.listText}>Conta BTG Banking (banco digital) gratuita.</span>
             </li>
           </ul>
